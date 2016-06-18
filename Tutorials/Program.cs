52eb7084229dd01eacd472858f72c479642a14ac
nopara73 <adam.ficsor73@gmail.com>
2016-06-18 21:48:18 +0800

    Part2Lesson1

--- Tutorials/Program.cs
+++ Tutorials/Program.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Security.Policy;
 using System.Threading;
 using HiddenBitcoin.DataClasses;
+using HiddenBitcoin.DataClasses.Balances;
+using HiddenBitcoin.DataClasses.Histories;
 using HiddenBitcoin.DataClasses.KeyManagement;
 using HiddenBitcoin.DataClasses.Monitoring;
 
@@ -17,8 +19,8 @@ namespace Tutorials
         private static void Main()
         {
             //Part1(); // Storing keys
-            //Part2Lesson1(); // Monitoring keys using HTTP
-            Part2Lesson2(); // Monitoring safes using HTTP
+            Part2Lesson1(); // Monitoring keys using HTTP
+            //Part2Lesson2(); // Monitoring safes using HTTP
 
             Console.ReadLine();
         }
@@ -107,63 +109,64 @@ namespace Tutorials
         {
             var network = Network.MainNet;
 
+            // HttpMonitor can monitor The Blockchain through HTTP (QBitNinja)
             var httpMonitor = new HttpMonitor(network);
 
-            var balanceInfo = httpMonitor.GetAddressBalanceInfo("1ENCTCkqoJqy2XZ2m2Dy1bRax7hsSnC5Fc");
-            Console.WriteLine(balanceInfo.Confirmed);
-            Console.WriteLine(balanceInfo.Unconfirmed);
+            // Checking address balances
+            AddressBalanceInfo balanceInfo = httpMonitor.GetAddressBalanceInfo("1ENCTCkqoJqy2XZ2m2Dy1bRax7hsSnC5Fc");
+            Console.WriteLine($"Address balance: {balanceInfo.Balance}"); // 0,05474889
+            Console.WriteLine($"Confirmed balance: {balanceInfo.Confirmed}"); // 0
+            Console.WriteLine($"Unconfirmed balance: {balanceInfo.Unconfirmed}"); // 0,05474889
 
-
-            //tx is exotic (has OP_RETURN)
-            var transactionInfo =
-                httpMonitor.GetTransactionInfo("8bae12b5f4c088d940733dcd1455efc6a3a69cf9340e17a981286d3778615684");
-            //tx is normal
-            //var transactionInfo = httpMonitor.GetTransactionInfo("8bbd7678d93da5da8736a84a69a1de83834bea732c65342687e8db549f153504");
-
-            Console.WriteLine("txid: " + transactionInfo.Id);
-            Console.WriteLine("Network: " + transactionInfo.Network);
-            Console.WriteLine("Confirmed: " + transactionInfo.Confirmed);
-            Console.WriteLine("Total amount of all inputs: " + transactionInfo.TotalInputAmount);
-            Console.WriteLine("Total amount of all outputs: " + transactionInfo.TotalOutputAmount);
-            Console.WriteLine("Fee : " + transactionInfo.Fee);
-            Console.WriteLine("There are no exotic inputs or outputs, so all of them have been added successfully: "
-                              + Environment.NewLine + transactionInfo.AllInOutsAdded);
-
-            Console.WriteLine(Environment.NewLine + "Input addresses and amounts: ");
-            foreach (var input in transactionInfo.Inputs)
-            {
-                Console.WriteLine(input.Amount + " " + input.Address);
-            }
-            Console.WriteLine(Environment.NewLine + "Output addresses and amounts: ");
-            foreach (var output in transactionInfo.Outputs)
-            {
-                Console.WriteLine(output.Amount + " " + output.Address);
-            }
-
-            //lot of transactions:
-            var address = "13eh4wPLe1nCsh8FXJNpL6e9D1edWNT1Ub";
-            //only few transactions:
-            //var address = "19V1JJ68Ee57tKnG7NikH4tU93xqMShCyD";
-            var history =
-                httpMonitor.GetAddressHistory(address);
+            // Get history of an address
+            AddressHistory history = httpMonitor.GetAddressHistory("1ENCTCkqoJqy2XZ2m2Dy1bRax7hsSnC5Fc");
 
             Console.WriteLine("Number of transactions: " + history.Records.Count);
 
+            // Exercise: are all transaction confirmed?
             var allTransactionsConfirmed = true;
-            foreach (var record in history.Records)
+            foreach (AddressHistoryRecord record in history.Records)
             {
                 Console.WriteLine(record.TransactionId + " : " + record.Amount);
                 allTransactionsConfirmed = allTransactionsConfirmed && record.Confirmed;
             }
             Console.WriteLine("All transactions are confirmed: " + allTransactionsConfirmed);
 
+            // Exercise: get the balance of the address
             Console.WriteLine("Total received - Total spent = Balance");
             Console.WriteLine(history.TotalReceived + " - " + history.TotalSpent + " = " +
-                              (history.TotalReceived - history.TotalSpent));
+                                (history.TotalReceived - history.TotalSpent));
 
-            balanceInfo = httpMonitor.GetAddressBalanceInfo(address);
-            Console.WriteLine(@"httpMonitor.GetAddressBalanceInfo(address): " +
-                              (balanceInfo.Confirmed + balanceInfo.Unconfirmed));
+            // Get some data from the transaction
+            TransactionInfo transactionInfo1 = httpMonitor.GetTransactionInfo(history.Records.First().TransactionId);
+            
+            Console.WriteLine("txid: " + transactionInfo1.Id);
+            Console.WriteLine("Network: " + transactionInfo1.Network);
+            Console.WriteLine("Confirmed: " + transactionInfo1.Confirmed);
+            Console.WriteLine("Total amount of all inputs: " + transactionInfo1.TotalInputAmount);
+            Console.WriteLine("Total amount of all outputs: " + transactionInfo1.TotalOutputAmount);
+            Console.WriteLine("Fee : " + transactionInfo1.Fee);
+
+            Console.WriteLine(Environment.NewLine + "Input addresses and amounts: ");
+            foreach (var input in transactionInfo1.Inputs)
+            {
+                Console.WriteLine(input.Amount + " " + input.Address);
+            }
+            Console.WriteLine(Environment.NewLine + "Output addresses and amounts: ");
+            foreach (var output in transactionInfo1.Outputs)
+            {
+                Console.WriteLine(output.Amount + " " + output.Address);
+            }
+
+            // Sometimes my API can't fully process a transaction, because it has OP_RETURN for example
+            // It should not be a concern for a Bitcoin wallet that purely handles money, if a transaction output or input has not been added
+            // that means it has some other purpose, a wallet API can dismiss it
+            // This tx is exotic (has OP_RETURN)
+            TransactionInfo transactionInfo2 =
+                httpMonitor.GetTransactionInfo("8bae12b5f4c088d940733dcd1455efc6a3a69cf9340e17a981286d3778615684");
+            Console.WriteLine(transactionInfo2.Id);
+            Console.WriteLine("There are exotic inputs or outputs, so not all of them have been added successfully: "
+                                + Environment.NewLine + !transactionInfo2.AllInOutsAdded);
         }
 
         private static void Part2Lesson2()
